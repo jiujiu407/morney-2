44243beccbbeca255cdcffb2461049fbd9e19c9d
jiujiu407 <1503874780@qq.com>
2021-04-08 11:23:07 +0800

    删除tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,13 +22,21 @@ const useTags = () =>{
         return result;
     };
     const updateTag = (id:number,obj:{name:string})=>{
+        //获取你要改的下标
         const index = findTagIndex(id);
         //深拷贝tags得到tagsClone
         const tagsClone = JSON.parse(JSON.stringify(tags));
         //把tagsClone的第index删掉，换成{id:id,name:obj.name}
         tagsClone.splice(index,1,{id:id,name:obj.name});
         setTags(tagsClone);
+    };
+    const deleteTag = (id:number)=>{
+        const index = findTagIndex(id);
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        //把tagsClone的第index删掉，换成{id:id,name:obj.name}
+        tagsClone.splice(index,1);
+        setTags(tagsClone);
     }
-    return {tags, setTags,findTag,updateTag,findTagIndex};
+    return {tags, setTags,findTag,updateTag,findTagIndex,deleteTag};
 };
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -19,10 +19,30 @@ line-height: 20px;
 padding: 14px;
 background: white;
 `;
+const InputWrapper = styled.div`
+background: white;
+padding: 0 16px;
+margin-top: 16px;
+`;
 const Tag:React.FC=()=>{
-    const {findTag,updateTag} = useTags()
+    const {findTag,updateTag,deleteTag} = useTags()
     let {id:idString} = useParams<Params>();
     const tag = findTag(parseInt(idString));
+    const tagContent =(tag:{id:number; name:string})=>(
+        <div>
+            <InputWrapper>
+                <Input label="标签名" type="text" placeholder="标签名"
+                       value={tag.name} onChange={(e) => {
+                    updateTag(tag.id,{name:e.target.value});}}></Input>
+            </InputWrapper>
+            <Center>
+                <Space/>
+                <Space/>
+                <Space/>
+                <Button onClick={()=>deleteTag(tag.id)}>删除标签</Button>
+            </Center>
+        </div>
+    );
     return(
         <Layout>
             <Topbar>
@@ -30,18 +50,9 @@ const Tag:React.FC=()=>{
                 <span>编辑标签</span>
                 <Icon />
             </Topbar>
-            <div>
-                <Input label="标签名" type="text" placeholder="标签名"
-                       value={tag.name} onChange={(e) => {
-                          updateTag(tag.id,{name:e.target.value});}}></Input>
-            </div>
-            <Center>
-                <Space/>
-                <Space/>
-                <Space/>
-                <Button>删除标签</Button>
-            </Center>
+            {tag ?  tagContent(tag): <Center>tag不存在</Center>}
         </Layout>
     )
-}
+
+};
 export {Tag}
