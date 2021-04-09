cca2aebe4d727ec11d3e829602f2b8923bd3f258
jiujiu407 <1503874780@qq.com>
2021-04-09 11:30:52 +0800

    优化滚动逻辑

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Nav from "./Nav";
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import styled from "styled-components";
 const Wrapper = styled.div`
 min-height: 100vh;
@@ -11,14 +11,30 @@ border:1px solid green;
 flex-grow: 1;
 overflow: auto;
 `;
-const Layout = (props:any)=>{
+type Props = {
+    className?:string;
+    scrollTop?:number;
+}
+const Layout: React.FC<Props> = (props)=>{
+    const mainRef = useRef<HTMLDivElement>(null);
+    useEffect(()=>{
+        setTimeout(()=>{
+            if(!mainRef.current){return;}
+            //  props.scrollTop加！叫ts不要管那么多，反正我不会让他为空
+            mainRef.current.scrollTop = props.scrollTop!;
+        },0);
+    },[props.scrollTop]);
     return(
         <Wrapper>
-            <Main className={props.className}>
+            <Main ref={mainRef} className={props.className} data-x={'frank'}>
                 {props.children}
             </Main>
             <Nav/>
         </Wrapper>
     )
+};
+//给它一个默认值
+Layout.defaultProps={
+    scrollTop:0
 }
 export default Layout;
